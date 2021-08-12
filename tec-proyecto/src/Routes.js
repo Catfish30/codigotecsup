b7c12a34e25990b2779f36beb1f43abe4c930b57
Catfish30 <michael.huamani@tecsup.edu.pe>
2021-08-12 00:53:43 -0500

    update

--- tec-proyecto/src/Routes.js
+++ tec-proyecto/src/Routes.js
@@ -4,7 +4,11 @@ import CursosViews from './views/CursosViews'
 import CrearCursoView from './views/CrearCursoView'
 import EditarCursoView from './views/EditarCursoView'
 import NotasView from './views/NotasView'
+
 import LoginView from './views/LoginView'
+
+import CursosViewAlum from './views/CursosViewAlum'
+import NotasViewAlum from './views/NotasViewAlum'
 
 export default function Routes() {
     return (
@@ -14,6 +18,8 @@ export default function Routes() {
             <Route path='/admin/crear' exact component={CrearCursoView} />
             <Route path="/admin/editar/:id" exact component={EditarCursoView} />
             <Route path="/admin/cursos/:id" exact component={NotasView} />
+            <Route path="/alumnos" exact component={CursosViewAlum} />
+            <Route path="/alumnos/cursos/:id" exact component={NotasViewAlum} />
         </div>
     )
 }
--- tec-proyecto/src/views/LoginView.js
+++ tec-proyecto/src/views/LoginView.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function LoginView() {
     return (
         <div className='container p-4' style={{minHeight:'70vh'}}>
-            <h1 className="text-center">Login</h1>
+            <h1 className="text-center mb-5">Login</h1>
+            <div className="row justify-content-center">
+                <div className="col-3 ms-5">
+                    <Link className='btn btn-dark' to="/alumnos">
+                        Alumnos
+                    </Link>
+                </div>
+                <div className="col-2">
+                    <Link className='btn btn-dark' to='admin'>
+                        Administrador
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
